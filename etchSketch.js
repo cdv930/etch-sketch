84c60c8a17f45c193cf309c478415c204fed8e60
cdv930 <47964627+cdv930@users.noreply.github.com>
2025-09-20 02:07:06 -0700

    feat. add grid size adjust button

--- etchSketch.js
+++ etchSketch.js
@@ -26,11 +26,35 @@ function mkBoxes(sideLength) {
     const mkBox = document.createElement("div");
     mkBox.classList.add("box");
     mkBox.id =`${i},${z}`;
-    mkBox.addEventListener('mouseenter',() => mkBox.classList.add("colored"));
+    mkBox.addEventListener('mouseenter',() => mkBox.style.backgroundColor='black');
     currentRow.appendChild(mkBox);   
     }
     }
 }
 
-mkRows(initialResolution);
-mkBoxes(initialResolution);
+function makeDisplay(length){
+    mkRows(length);
+    mkBoxes(length);
+}
+
+makeDisplay(initialResolution);
+
+
+const referenceHeader = document.querySelector("header");
+
+const resizeBtn = document.createElement("button");
+ 
+resizeBtn.textContent='Grid Size Adjust';
+resizeBtn.addEventListener('click', () => {
+    let whatSize = prompt("To resize the grid, input a positive integer value less than or equal to 100.");
+        if (whatSize === null){
+            alert("fine.keep.it.");}
+        else if (whatSize <1 || whatSize>100){
+            alert("Try again. Enter a valid integer please.")
+        }
+        else{displayWindow.replaceChildren();
+             makeDisplay(whatSize)};
+        }
+        );
+referenceHeader.appendChild(resizeBtn);
+
